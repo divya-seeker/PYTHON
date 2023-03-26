f8e4018d665c7b88bccb380329156a0b7638307c
Divya Dwivedi <divya.dwivedi.mnnit@gmail.com>
2023-03-27 03:10:47 +0530

    initial commit

---
+++ dataTypes.py
@@ -0,0 +1,14 @@
+name="Divya Dwivedi"
+print(name,type(name))
+
+age=20
+print(age,type(age))
+
+isPassed=True
+print(isPassed,type(isPassed))
+
+my_complex_number=3+5j
+print(my_complex_number,type(my_complex_number))
+
+fruits_list=["Apple","Orange","Banana"]
+print(fruits_list,type(fruits_list))
